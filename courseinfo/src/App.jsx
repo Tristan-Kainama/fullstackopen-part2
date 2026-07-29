4d6c848d540a78218b3076f6993a76a222216155
baveryk <io.kainama@gmail.com>
2026-07-29 15:26:21 +0700

    Finished exercise 2.2 and 2.3

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -16,12 +16,18 @@ const Part = (props) => (
 
 const Total = (props) => <p>Number of exercises {props.total}</p>
 
-const Course = (props) => (
+const Course = (props) => {
+    const total = props.course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
+    console.log(total)
+
+    return(
     <div>
         <Header course={props.course.name}/>
         <Content parts={props.course.parts}/>
+        <Total total={total}/>
     </div>
-)
+    )
+}
 
 const App = () => {
   const course = {
