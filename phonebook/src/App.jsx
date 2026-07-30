e288af5896c3a5df200b2c8338f9dcad6f854753
baveryk <io.kainama@gmail.com>
2026-07-30 16:02:01 +0700

    Finished exercise 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Filter = (props) => {
   return (
@@ -39,17 +41,21 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState([])
+  const [filteredPersons, setFilteredPersons] = useState([]) 
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+    })
+  }, [])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
-  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const handleSearch = (event) => {
     const newSearch = event.target.value
