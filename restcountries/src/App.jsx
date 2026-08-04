af8146d605fd94db65ce36c1966e7752872e60eb
baveryk <io.kainama@gmail.com>
2026-08-04 22:49:15 +0700

    Finished exercise 2.18

---
+++ restcountries/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import countriesService from './services/countries.js'
+
+function SearchForm({ search, handleCountryChange }) {
+  return (
+    <form>
+      <div>
+        find countries <input value={search} onChange={handleCountryChange} />
+      </div>
+    </form>
+  )
+}
+
+function Output({ filteredAmount, filteredCountries }) {
+  if (filteredAmount > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  if (filteredAmount > 1) {
+    return (
+      <>
+        {filteredCountries.map((country) => (
+          <div key={country.name.common}>
+            <p>{country.name.common}</p>
+          </div>
+        ))}
+      </>
+    )
+  }
+
+  if (filteredAmount === 1) {
+    const country = filteredCountries[0]
+
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>Capital {country.capital?.join(', ')}</p>
+        <p>Area {country.area}</p>
+        <h3>Languages</h3>
+        <ul>
+          {Object.entries(country.languages ?? {}).map(([code, name]) => (
+            <li key={code}>{name}</li>
+          ))}
+        </ul>
+        <img src={country.flags?.png} alt={`Flag of ${country.name.common}`} width="150" />
+      </div>
+    )
+  }
+
+  return <p>No countries found</p>
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    countriesService.getAll().then((initialCountries) => {
+      setCountries(initialCountries)
+      setFilteredCountries(initialCountries)
+    })
+  }, [])
+
+  const handleCountryChange = (event) => {
+    const newSearch = event.target.value
+    setSearch(newSearch)
+
+    const nextFilteredCountries = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(newSearch.toLowerCase())
+    )
+
+    setFilteredCountries(nextFilteredCountries)
+  }
+
+  const filteredAmount = filteredCountries.length
+
+  return (
+    <div>
+      <SearchForm search={search} handleCountryChange={handleCountryChange} />
+      <Output filteredAmount={filteredAmount} filteredCountries={filteredCountries} />
+    </div>
+  )
+}
+
+export default App
